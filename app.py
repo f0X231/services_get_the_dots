5acc08b3b016e4e820bf62b66af1affcbfc5ca7c
zhaii <apichai.s@thairathonline.co.th>
2024-12-16 14:42:14 +0700

    multiple upload

--- app.py
+++ app.py
@@ -51,50 +51,84 @@ def get():
 def upload():
     # Check if the post request has the file part
     if request.method == 'POST':
-        if 'file' not in request.files:
-            return 'No file part'
-        
-        file = request.files['file']
-        # If the user does not select a file, the browser submits an
-        # empty file without a filename.
-        if file.filename == '':
+        files = request.files.getlist('files[]')
+        file_count = len(files)
+
+        if file_count <= 0:
             return 'No selected file'
+
+        listFileName = []
+        for file in files:
+            # file.save(f"uploads/{file.filename}")
+            # file = request.files['file']
+            # If the user does not select a file, the browser submits an
+            # empty file without a filename.
+            if file.filename == '':
+                return 'No selected file'
         
-        if file and allowed_image_file(file.filename):
-            folder_path = create_date_folder(BASE_DIRECTORY_UPLOAD_FOLDER)
-            new_filename = str(uuid.uuid4())
-            unique_filename = new_filename + os.path.splitext(secure_filename(file.filename))[1]
-            file_path = os.path.join(folder_path, unique_filename)
-            file.save(file_path)
+            if file and allowed_image_file(file.filename):
+                folder_path = create_date_folder(BASE_DIRECTORY_UPLOAD_FOLDER)
+                new_filename = str(uuid.uuid4())
+                unique_filename = new_filename + os.path.splitext(secure_filename(file.filename))[1]
+                file_path = os.path.join(folder_path, unique_filename)
+                file.save(file_path)
 
-            # Split the image into six parts
-            split_folder_path = os.path.join(BASE_DIRECTORY_RESULT_FOLDER, new_filename)
-            os.makedirs(split_folder_path, exist_ok=True)
-            list_of_split_image = split_image(file_path, split_folder_path)
+                # Split the image into six parts
+                split_folder_path = os.path.join(BASE_DIRECTORY_RESULT_FOLDER, new_filename)
+                os.makedirs(split_folder_path, exist_ok=True)
+                list_of_split_image = split_image(file_path, split_folder_path)
 
-            # Count the number of red, green, and overlapping dots in each split image
-            result = []
-            dictKey = ["red", "green", "overlapping", "image"]
-            length = len(list_of_split_image)
-            for i in range(length):
-                countRedDot = count_red_dots(list_of_split_image[i])
-                countGreenDot = count_green_dots(list_of_split_image[i])
-                countOverlapping = count_overlapping_green_red_dots(list_of_split_image[i])
-                dots = [countRedDot, countGreenDot, countOverlapping, list_of_split_image[i]]
-                result.append(dict(zip(dictKey, dots)))  
+                # Count the number of red, green, and overlapping dots in each split image
+                result = []
+                dictKey = ["red", "green", "overlapping", "image"]
+                length = len(list_of_split_image)
+                for i in range(length):
+                    countRedDot = count_red_dots(list_of_split_image[i])
+                    countGreenDot = count_green_dots(list_of_split_image[i])
+                    countOverlapping = count_overlapping_green_red_dots(list_of_split_image[i])
+                    dots = [countRedDot, countGreenDot, countOverlapping, list_of_split_image[i]]
+                    result.append(dict(zip(dictKey, dots)))  
 
-            log_to_daily_file(new_filename, result)
-            return redirect(url_for('proof', id=new_filename))
+                log_to_daily_file(new_filename, result)
+                listFileName.append(new_filename)
+
+        paramURI = ''
+        if len(listFileName) > 1:
+            paramURI = ','.join(listFileName)
+        else:
+            paramURI = listFileName[0]
+        return redirect(url_for('proof', id=paramURI))
     else:
         return render_template('upload.html')
 
 
 @app.route('/proof/<string:id>', methods=['GET', 'POST'])
 def proof(id):
-    logFile = "logs/log_" + id + ".json"
-    if is_file_exists(logFile):
-        data = read_json_file(logFile)
-        return render_template('checklist.html', data={"id":id, "results":data})
+    if id != "":
+        logid = id.split(',')
+        if len(logid) > 1:
+            chkFileInFolder = 0
+            listFileName = []
+            dataOverall = []
+            for filename in logid:
+                logFile = "logs/log_" + filename + ".json"
+                if is_file_exists(logFile):
+                    chkFileInFolder += 1
+                    listFileName.append(filename)
+                    data = read_json_file(logFile)
+                    dataOverall.append(data)
+            if chkFileInFolder == len(logid):
+                print(dataOverall)
+                return render_template('checklist.html', data={"id":listFileName, "results":dataOverall, "length": len(logid)})
+            else:
+                return redirect(url_for('upload'))
+        else:
+            logFile = "logs/log_" + id + ".json"
+            if is_file_exists(logFile):
+                data = read_json_file(logFile)
+                return render_template('checklist.html', data={"id":id, "results":data, "length": 1})
+            else:
+                return redirect(url_for('upload'))
     else:
         return redirect(url_for('upload'))
 
@@ -102,26 +136,33 @@ def proof(id):
 @app.route('/save', methods=['POST'])
 def save():
     if request.method == 'POST':
-        dataId = request.form.get('fileid')
-        dataImage = request.form.getlist('imagepath[]')
-        dataRed = request.form.getlist('red[]')
-        dataGreen = request.form.getlist('green[]')
-        dataOverlap = request.form.getlist('overlap[]')
-
-        create_date_folder(BASE_DIRECTORY_LRESULT_FOLDER + '/' + dataId)
-        # save new image with text number dot
-        result = []
-        dictKey = ["red", "green", "overlapping", "original", "result"]
-        length = len(dataImage)
-        for i in range(length):
-            newPathImage = dataImage[i].replace(BASE_DIRECTORY_RESULT_FOLDER, "")
-            picResult = BASE_DIRECTORY_LRESULT_FOLDER + newPathImage
-            write_text_on_image(dataImage[i], picResult, dataOverlap[i], dataGreen[i], dataRed[i])
-            dots = [int(dataRed[i]), int(dataGreen[i]), int(dataOverlap[i]), dataImage[i], picResult]
-            result.append(dict(zip(dictKey, dots)))
-
-        log_to_daily_file(dataId, result)
-        return jsonify({"id": dataId, "data": result})
+        dataId = request.form.getlist('fileid[]')
+        dataImage = {}
+        dataRed = {}
+        dataGreen = {}
+        dataOverlap = {}
+        resultOverall = []
+        for l in range(len(dataId)):
+            inxDataId = l
+            dataImage[inxDataId] = request.form.getlist(f'imagepath[{inxDataId}][]')
+            dataRed[inxDataId] = request.form.getlist(f'red[{inxDataId}][]')
+            dataGreen[inxDataId] = request.form.getlist(f'green[{inxDataId}][]')
+            dataOverlap[inxDataId] = request.form.getlist(f'overlap[{inxDataId}][]')
+            create_date_folder(BASE_DIRECTORY_LRESULT_FOLDER + '/' + dataId[inxDataId])
+            # save new image with text number dot
+            result = []
+            dictKey = ["red", "green", "overlapping", "original", "result"]
+            length = len(dataImage[inxDataId])
+            for i in range(length):
+                newPathImage = dataImage[inxDataId][i].replace(BASE_DIRECTORY_RESULT_FOLDER, "")
+                picResult = BASE_DIRECTORY_LRESULT_FOLDER + newPathImage
+                write_text_on_image(dataImage[inxDataId][i], picResult, dataOverlap[inxDataId][i], dataGreen[inxDataId][i], dataRed[inxDataId][i])
+                dots = [int(dataRed[inxDataId][i]), int(dataGreen[inxDataId][i]), int(dataOverlap[inxDataId][i]), dataImage[inxDataId][i], picResult]
+                result.append(dict(zip(dictKey, dots)))
+            tmpData = {"id": dataId[inxDataId], "data": result}
+            resultOverall.append(tmpData)
+            log_to_daily_file(dataId[inxDataId], result)
+        return jsonify(resultOverall)
     else:
         return redirect(url_for('upload'))
 
